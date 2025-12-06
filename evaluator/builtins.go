898a4ccbf85c0d2cfbde681f94950c84da122a50
valkyraycho <razor0424@gmail.com>
2025-12-06 12:50:01 +0800

    [feat] add builtin function puts

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/valkyraycho/interpreter/object"
+import (
+	"fmt"
+
+	"github.com/valkyraycho/interpreter/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {Fn: func(args ...object.Object) object.Object {
@@ -81,5 +85,11 @@ var builtins = map[string]*object.Builtin{
 		copy(newElements, arr.Elements)
 		newElements[length] = args[1]
 		return &object.Array{Elements: newElements}
+	}},
+	"puts": {Fn: func(args ...object.Object) object.Object {
+		for _, arg := range args {
+			fmt.Println(arg.Inspect())
+		}
+		return NULL
 	}},
 }
